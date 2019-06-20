c86d243f1a4c0965bdf8f6fb0a8cc85f2b93fbe7
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-20 20:17:06 +0530

    Setup of project completed

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+export default class App extends Component {
+  render(){
+    return(
+    <div>
+      <h1>
+        Welcome to App component.
+      </h1>
+    </div>
+    );
+  }
+}
