bc5050190f97ad59dfe1a9bd91147bd176150709
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-22 22:07:22 +0530

    Done for heroku

--- app.js
+++ app.js
@@ -46,6 +46,15 @@ app.use('/api/auth',auth);
 app.use('/api/profile',profile);
 
 
+// serving all the assets
+if(process.env.NODE_ENV==='production'){
+app.use(express.static('client/build'));
+app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+});
+}
+
+
 // starting the server
 app.listen(port,()=>console.log(`Server is running at port ${port}`));
 
